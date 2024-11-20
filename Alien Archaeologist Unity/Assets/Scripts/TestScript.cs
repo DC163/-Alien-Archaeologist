2efd4aadc87013e5f2ebcc5834ac3bf5b63ca7a1
DC163 <1637811@fife.ac.uk>
2024-11-20 10:48:44 +0000

    final commitments
    commiting to the bitdkg

--- Alien Archaeologist Unity/Assets/Scripts/TestScript.cs
+++ Alien Archaeologist Unity/Assets/Scripts/TestScript.cs
@@ -7,7 +7,14 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //the humble first line
         Debug.Log("jorking mm");
+
+        /* this is the second line
+         * and this is the third */
+        Debug.Log("You have my sword.");
+        Debug.LogWarning("And my bow.");
+        Debug.LogError("And no bitches.");
         transform.Rotate(0, 0, 180);
     }
 
