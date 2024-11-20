52b3ea18a0263f5903c9eec3737e96a226856308
DC163 <1637811@fife.ac.uk>
2024-11-20 10:20:13 +0000

    DOG SPRITE
    BIG RETARDED LOOKING DOG HAS BEEN ADDED

--- Alien Archaeologist Unity/Assets/Scripts/TestScript.cs
+++ Alien Archaeologist Unity/Assets/Scripts/TestScript.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("jorking mm");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("update: still jorking!");
     }
 }
